a3a5aa778f5137763bff0750f8bf01ac23d41a93
Roshan Pisharody <r@shanp.fyi>
2024-01-07 23:34:19 +0530

    Day 003 - Allocating a Backbuffer

--- build.zig
+++ build.zig
@@ -7,14 +7,14 @@ pub fn build(b: *std.Build) void {
 
     const win32 = b.addModule("win32", .{ .source_file = .{ .path = "lib/zigwin32/win32.zig" } });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "handmade",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    lib.addModule("win32", win32);
-    b.installArtifact(lib);
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "handmade",
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // lib.addModule("win32", win32);
+    // b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "handmade",
