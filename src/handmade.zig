bf511cd58be31af7b4cc2e7c73adb17e9c8ddaad
Roshan Pisharody <r@shanp.fyi>
2024-01-27 16:32:47 +0530

    ported the current sound setup to be platform independent

--- src/handmade.zig
+++ src/handmade.zig
@@ -1,5 +1,14 @@
+///TODO(rosh): Services that platform layer provides to the game
+///NOTE(rosh): Services that game provides to the platform layer.
+/// (this may expand in the future - sound on seperate thread, etc.)
+///
+/// FOUR THINGS - timing, controller/keyboard input, bitmap buffer to use, sound buffer to use
+///
+/// TODO(rosh): In the future, rendering __specifically__ will become a three-tiered abstraction!!!!
+///
+const std = @import("std");
+const math = std.math;
 pub const game_offscreen_buffer = struct {
-   
     memory: ?*anyopaque = null,
     width: i32 = undefined,
     height: i32 = undefined,
@@ -7,7 +16,31 @@ pub const game_offscreen_buffer = struct {
     bytesPerPixel: u8 = 4,
 };
 
-pub fn GameUpdateAndRender(buffer: *game_offscreen_buffer, blueOffset: i32, greenOffset: i32) void {
+pub const game_sound_output_buffer = struct {
+    samples: [*]i16 = undefined,
+    samplesPerSecond: u32 = undefined,
+    samplesCount: u32 = undefined,
+};
+
+var tSine: f32 = undefined;
+fn GameOutputSound(soundBuffer: *game_sound_output_buffer, toneHz: u32) void {
+    const toneVolume: u32 = 3000;
+    var sampleOut = soundBuffer.samples;
+    const wavePeriod: u32 = soundBuffer.samplesPerSecond / toneHz;
+    // var sampleOut: [*]i16 = @ptrCast(@alignCast(ptr));
+    for (0..soundBuffer.samplesCount) |_| {
+        const sineValue = math.sin(tSine);
+        const sampleValue: i16 = @intFromFloat(sineValue * @as(f32, @floatFromInt(toneVolume)));
+
+        sampleOut[0] = sampleValue;
+        sampleOut += @as(usize, 1);
+        sampleOut[0] = sampleValue;
+        sampleOut += @as(usize, 1);
+
+        tSine += 2.0 * math.pi * 1.0 / @as(f32, @floatFromInt(wavePeriod));
+    }
+}
+fn RenderWeirdGradient(buffer: *game_offscreen_buffer, blueOffset: i32, greenOffset: i32) void {
     var row: [*]u8 = @ptrCast(buffer.*.memory);
     var y: u32 = 0;
     while (y < buffer.*.height) : (y += 1) {
@@ -25,4 +58,9 @@ pub fn GameUpdateAndRender(buffer: *game_offscreen_buffer, blueOffset: i32, gree
         }
         row += buffer.*.pitch;
     }
+}
+pub fn GameUpdateAndRender(buffer: *game_offscreen_buffer, blueOffset: i32, greenOffset: i32, soundbuffer: *game_sound_output_buffer, toneHz: u32) void {
+    //TODO(rosh): Allow sample offsets here for more robust platform options
+    GameOutputSound(soundbuffer, toneHz);
+    RenderWeirdGradient(buffer, blueOffset, greenOffset);
 }
